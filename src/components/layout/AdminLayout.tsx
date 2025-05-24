84b19b7993a006fbfd0adfe43d4daba399e0b18c
Marko <marko@gigrove.com>
2025-05-24 07:31:35 +0200

    sidebard

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -109,7 +109,7 @@ export const AdminLayout = ({ children }: AdminLayoutProps) => {
 return (
     <div className="flex min-h-screen bg-background">
       <AdminSidebar />
-      <main className="flex-1 pl-32 p-6 overflow-auto">
+      <main className="flex-1 pl-64 p-6 overflow-auto">
         {children}
       </main>
     </div>
