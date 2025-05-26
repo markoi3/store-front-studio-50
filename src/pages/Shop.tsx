8cfaad2d17ea3c254a35a155dd91e97106d917fe
Marko <marko@gigrove.com>
2025-05-26 19:15:40 +0200

    Update Shop.tsx

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { ProductCard, Product } from "@/components/shop/ProductCard";
+import { ShopLayout } from "@/components/layout/ShopLayout";
 import { useSearchParams } from "react-router-dom";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
